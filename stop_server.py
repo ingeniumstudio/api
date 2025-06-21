b99279b5bd4c24f5e64aa8e2c4c5248bb16a9f13
yk <117122269+ingeniumstudio@users.noreply.github.com>
2025-06-21 05:39:39 -0300

    commmit

--- stop_server.py
+++ stop_server.py
@@ -12,10 +12,12 @@ def do_reboot():
     pid_list = [pid.split('=')[1] for pid in ss_process.stdout.split(',')
                 if pid.startswith("pid=")]
 
-    for pid in sorted(pid_list):  # lesser pid first in list
-        subprocess.run(args_kill + [pid])
+    #  for pid in sorted(pid_list):  # lesser pid first in list
+    #      subprocess.run(args_kill + [pid])
+    #
+    #  return ", ".join(pid_list)
 
-    return ", ".join(pid_list)
+    subprocess.run(args_kill + pid_list)
 
 if __name__ == "__main__":
     do_reboot()
