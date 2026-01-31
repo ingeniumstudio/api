6c34b7c36646de1b2446ac0a49ebbacbc2bdca76
yk <117122269+ingeniumstudio@users.noreply.github.com>
2026-01-31 07:10:11 -0300

    commit

--- functions.py
+++ functions.py
@@ -125,7 +125,7 @@ def text_to_image(text: str,
 
 #new
 def verify_github_signature(data_bytes, webhook_secret, signature):
-    _algo, provided_hash = signature.split('=')
+    __algo, provided_hash = signature.split('=')
     provided_hash = signature.split('=')
 
     computed_hash = hmac.new(key=webhook_secret.encode("utf-8"),
